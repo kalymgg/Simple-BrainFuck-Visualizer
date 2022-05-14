4877264c23883c0c62580c994b8b8c5193ba72db
kalymgg <35005603+kalymgg@users.noreply.github.com>
2022-05-14 15:39:53 +0400

    typo

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ from bfMachine import bfMachine as machine
 
 code = '>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<++++++++>-]<-.--------.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.'
 
-machine = bfMachine(code, verbose = True, delay=0.05, clean = True)
+machine = machine(code, verbose = True, delay=0.05, clean = True)
 
 machine.run()
